17ce1e3e6e944d5e021a79ce753bfd547a1aabe6
hyobin28 <biny0328@naver.com>
2025-03-23 02:08:14 +0900

    홈 화면 레이아웃 구성

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,18 @@ import { View, Text, StyleSheet } from "react-native";
 const HomeScreen = () => {
   return(
     <View style={styles.container}> 
-      <Text>홈화면이에용</Text>
+      
+      <View style={styles.petList}>
+        <Text>프로필</Text>
+      </View>
+
+      <View style={styles.matching}>
+        <Text>산책 매칭</Text>
+      </View>
+
+      <View style={styles.community}>
+        <Text>커뮤니티</Text>
+      </View>
     </View>
   )
 }
@@ -12,6 +23,28 @@ const HomeScreen = () => {
 styles = StyleSheet.create({
   container: {
     flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "white", 
+    paddingBottom: 50,
+  }, 
+  petList: {
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "#FFF5E4"
+  }, 
+  matching: {
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "#FFF5E4"
+  }, 
+  community: {
+    flex: 2, 
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "#FFF5E4"
   }
 })
 
