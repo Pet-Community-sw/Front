ddc819f001a294931d0ea2319708e91c4b711d9c
hyobin28 <biny0328@naver.com>
2025-03-23 23:36:39 +0900

    프로필 api

---
+++ src/components/petProfile.js
@@ -0,0 +1,13 @@
+//홈 화면 펫 프로필
+import React from "react";
+import { View, Text } from "react-native";
+
+const PetProfile = () => {
+  return(
+    <View>
+
+    </View>
+  )
+}
+
+export default PetProfile;
