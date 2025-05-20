e233e448c81606d4c3ec871a41b4670fde71ce2e
hyobin28 <biny0328@naver.com>
2025-05-20 09:51:39 +0900

    웹소켓 API 연동

--- src/api/stompClient.js
+++ src/api/stompClient.js
@@ -12,7 +12,7 @@ global.WebSocket = w3cwebsocket;
 //메시지 구독, 전송, 연결 끊기 등 모든 행동의 중심 객체
 let stompClient = null;  
 
-//웹소켓 서버에 연결할 때 호출됨
+//웹소켓 서버 연결
 export const connectStomp = async (onConnect) => {
   const token = await AsyncStorage.getItem("accessToken");
 
@@ -30,4 +30,33 @@ export const connectStomp = async (onConnect) => {
 
   stompClient.activate();   //연결 시작, new WebSocket(url)
   return stompClient;
+};
+
+//채팅방 구독 (사용자가 채팅방에 입장 시, 서버로부터 오는 메시지 실시간으로 받음)
+export const subscribeChat = (chatRoomId, onMessage) => {
+  if(!stompClient) return;
+
+  stompClient.subscribe(`/sub/chat/${chatRoomId}`, (msg) => {
+    const payload = JSON.parse(msg.body);
+    onMessage(payload);
+  });
+}
+
+//메시지 전송, message: 서버에 보내는 객체
+export const sendChat = (message) => {
+  if(!stompClient) return;
+
+  stompClient.publish({   //메시지 서버로 보냄
+    destination: "/pub/chat/message", 
+    body: JSON.stringify(message),    //전송할 실제 데이터
+  });
+};
+
+//연결 해제
+export const disconnectStomp = () => {
+  if(stompClient) {
+    stompClient.deactivate();
+    stompClient = null;
+    console.log("웹소켓 연결 해제")
+  };
 };
