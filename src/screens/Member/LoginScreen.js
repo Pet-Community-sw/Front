a68433d618de41d792265fc796189e13b8b94cf6
hyobin28 <hyobiny0328@gmail.com>
2025-10-29 23:48:16 +0900

    fix: navigate 오류 수정

--- src/screens/Member/LoginScreen.js
+++ src/screens/Member/LoginScreen.js
@@ -24,9 +24,6 @@ const LoginScreen = () => {
           console.log("✅ memberId 저장 완료:", data.memberId);
 
           await connectStomp(data.accessToken);
-          
-          // 로그인 성공 후 메인 화면으로 이동
-          // navigation.navigate(Home);
         },
 
         onError: (error) => {
